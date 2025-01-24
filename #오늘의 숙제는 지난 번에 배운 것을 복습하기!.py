37809b17aed2c3ba5104c972229d31090020cc58
eundan <monojude@gmail.com>
2025-01-24 19:03:32 +0900

    First commit

---
+++ #오늘의 숙제는 지난 번에 배운 것을 복습하기!.py
@@ -0,0 +1,7 @@
+#오늘의 숙제는 지난 번에 배운 것을 복습하기!
+
+question = input("Are you going to practice with Duolingo? ")
+if in "yes":
+    print ("Hell " + question) 
+if "no":
+    print ("Get lost!)")
---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+print("Hello, Victor!")
+answer = input("Why am I learning a computer language?")
+print(answer, "= I don't know yet.")
